d640134b71137ea1b269567b78c625b0e1e84579
Gameqwerty <nikita1907davidov@gmail.com>
2025-03-19 12:00:10 +0300

    Add files via upload

--- 7-1-measure.py
+++ 7-1-measure.py
@@ -1,42 +1,76 @@
-import RPi.GPIO as gpio
-import time
+import RPi.GPIO as GPIO
+import time 
 import matplotlib.pyplot as plt
-import matplotlib
-gpio.setmode(gpio.BCM)
-dac=[8, 11, 7 , 1, 0, 5, 12, 6]
-gpio.setup(dac, gpio.OUT)
-def dec2bin(n):
-    return [int(x) for x in ("0"*(8-len(bin(n)[2::]))+bin(n)[2::])]
+dac = [8,11,7,1,0,5,12,6]
+leds=[2,3,4,17,27,22,10,9]
 comp=14
 troyka=13
-gpio.setup(comp, gpio.IN)
-gpio.setup(troyka,gpio.OUT, initial=1)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac, GPIO.OUT)
+GPIO.setup(leds, GPIO.OUT)
+GPIO.setup(comp, GPIO.IN)
+GPIO.setup(troyka,GPIO.OUT,initial=GPIO.HIGH)
+def d2b(value):
+ return [int(bit) for bit in bin(value)[2:].zfill(8)]
+def n2d(v):
+    s=d2b(v)
+    #print(s)
+    GPIO.output(dac,s)
+    time.sleep(0.02)
+    return s
 def adc():
-        c=0
-        for k in range(8):
-            c+=2**(7-k)
-            gpio.output(dac, dec2bin(c))
-            time.sleep(0.001)
-            if gpio.input(comp)==1:
-                c-=2**(7-k)
-        return (c)
-value=[]
+    a=[]
+    val=0
+    for i in range(8):
+        #print(val)
+        s=n2d(val+2**(8-i-1)-1)
+        cv=GPIO.input(comp)
+        if cv==0:
+            val=val+2**(8-i-1)
+            a.append(1)
+            #GPIO.output(leds[i],1)
+        else :
+            a.append(0)
+            #GPIO.output(leds[i],0)
+
+        
+    vol=val*3.3/256
+    print(val,a,vol)
+    return val
 t0=time.time()
+f=0
+y=1
+value=[]
 try:
-    while adc()<206:
-        gpio.output(troyka, 1)
-        value.append(adc()*3.3/256)
-        print(adc())
-        
-    while adc()>192:
-        gpio.output(troyka, 0)
-        value.append(adc()*3.3/256)
-        print(adc())
-        
+    while f<190:
+        y=f
+        f=adc()
+        print(f)
+        value.append(f*3.3/256)
+        if f==0:
+            c=0
+        else:
+            c=int(f/32)+1
+        #print(c)
+        a=d2b(2**c-1)
+        GPIO.output(leds,a)
+    GPIO.output(troyka,0)
+    f=adc()
+    print(f)
+    while f>176:
+        f=adc()
+        value.append(f*3.3/256)
+        if f==0:
+            c=0
+        else:
+            c=int(f/32)+1
+        #print(c)
+        a=d2b(2**c-1)
+        GPIO.output(leds,a)
 finally:
+    GPIO.output(dac,0)
+    GPIO.cleanup()
     t1=time.time()
-    gpio.output(dac, 0)
-    gpio:gpio.cleanup()
     t=t1-t0
     T=t/len(value)
     f=1/T
